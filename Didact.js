b9f644f455c21c4da194c83a3723171f8b31d6ee
polyfog <98544265+polyfog@users.noreply.github.com>
2023-07-04 17:04:09 -0300

    creating the render function and the concurrent mode

--- Didact.js
+++ Didact.js
@@ -22,8 +22,46 @@ function createElementText(text) {
     }
 }
 
+function render(element, container) {
+    const dom = 
+        element.type == "TEXT_ELEMENT"
+        ? document.createTextNode("")
+        : document.createElement(element.type)
+
+    const isProperty = key => key !== "children"
+    Object.key(element.props)
+        .filter(isProperty)
+        .forEach(name => {
+            dom[name] = element.props[name]
+        })
+
+    element.props.children.forEach(child =>
+        render(child, dom)
+    )
+
+    container.appendChild(dom)
+}
+
+let nextUnitOfWork = null
+
+function workLoop(deadline) {
+    let shouldYield = false
+    while (nextUnitOfWork && !shouldYield) {
+        nextUnitOfWork = performUnitOfWork(
+            nextUnitOfWork
+        )
+        shouldYield = deadline.timeRemaining() < 1
+    }
+    requestIdleCallback(workLoop)
+}
+
+function performUnitOfWork(nextUnitOfWork) {
+    // TODO
+}
+
 const Didact = {
     createElement,
+    render,
 }
 
 /** @jsx Didact.createElement */
@@ -35,4 +73,4 @@ const element = Didact.createElement(
 )
 
 const container = document.getElementById("root")
-ReactDOM.render(element, container)
+Didact.render(element, container)
