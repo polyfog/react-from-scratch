e291b8b84c22fe2afd953c23cf96d4be4ebbdc32
polyfog <98544265+polyfog@users.noreply.github.com>
2023-07-04 16:38:02 -0300

    creating the createElement function

---
+++ Didact.js
@@ -0,0 +1,38 @@
+function createElement(type, props, ...children) {
+    return {
+        type,
+        props: {
+            ...props,
+            children: children.map(child =>
+                typeof child === object
+                ? child
+                : createTextElement(child)
+            ),
+        },
+    }
+}
+
+function createElementText(text) {
+    return {
+        type: "TEXT_ELEMENT",
+        props: {
+            nodeValue: text,
+            children: [],
+        },
+    }
+}
+
+const Didact = {
+    createElement,
+}
+
+/** @jsx Didact.createElement */
+const element = Didact.createElement(
+    "div",
+    { id: "foo"},
+    Didact.createElement("a", null, "bar"),
+    Didact.createElement("b")
+)
+
+const container = document.getElementById("root")
+ReactDOM.render(element, container)
