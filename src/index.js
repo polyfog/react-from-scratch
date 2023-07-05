369801c83d702a45c306c0264be2e3c29e14b791
polyfog <98544265+polyfog@users.noreply.github.com>
2023-07-05 18:17:20 -0300

    adding state (hooks)

--- src/index.js
+++ src/index.js
@@ -222,6 +222,49 @@ function performUnitOfWork(fiber) {
     } 
 }
 
+let wipFiber = null
+let hookIndex = null
+
+function updateFunctionComponent(fiber) {
+    wipFiber = fiber
+    hookIndex = 0
+    wipFiber.hooks = []
+    const children = [fiber.type(fiber.props)]
+    reconcileChildren(fiber, children)
+}
+
+function useState(initial) {
+    const oldHook = 
+        wipFiber.alternate &&
+        wipFiber.alternate.hooks &&
+        wipFiber.alternate.hooks[hookIndex]
+
+    const hook = {
+        state: oldHook ? oldHook.state : initial,
+        queue: [],
+    }
+
+    const actions = oldHook ? oldHook.queue : []
+    actions.forEach(action => {
+        hook.state = action(hook.state)
+    })
+
+    const setState = action => {
+        hook.queue.push(action)
+        wipRoot = {
+            dom: currentRoot.dom,
+            props: currentRoot.props,
+            alternate: currentRoot,
+        }
+        nextUnitOfWork = wipRoot
+        deletions = []
+    }
+
+    wipFiber.hooks.push(hook)
+    hookIndex++
+    return [hook.state]
+}
+
 function updateHostComponent(fiber) {
     const children = [fiber.type(fiber.props)]
     reconcileChildren(fiber, children)
@@ -308,11 +351,22 @@ const Didact = {
 
 /** @jsx Didact.createElement */
 
-function App(props) {
-    return <h1>Hi {props.name}</h1>
+function Counter() {
+    const [state, setState] = Didact.useState(1)
+    return (
+        <h1 onClick={() => setState(c => c + 1)}>
+            Count: {state}
+        </h1>
+    )
 }
 
-const element = <App name="foo" />
+const element = <Counter />
+
+// function App(props) {
+//     return <h1>Hi {props.name}</h1>
+// }
+
+// const element = <App name="foo" />
 
 // const element = Didact.createElement(
 //     "div",
